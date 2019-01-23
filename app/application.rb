758f8db6c9eaa42285331fd7f5e4bf7b4f0c0151
Aaron Berns <anberns@gmail.com>
2019-01-23 14:55:14 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,6 @@
 class Application
   
-  @@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
+  @@items = []
   
   def call(env)
     resp = Rack::Response.new 
@@ -8,9 +8,7 @@ class Application
     
     if req.path.match(/items/)
       item = req.path.split("/items/").last
-      item_instance = @@items.find {|i| i.name == item }
-      
-      if item_instance
+      if item_instance =@@items.find{|i| i.name == item_name}
         resp.write item_instance.price
         resp.status = 200
       else 
