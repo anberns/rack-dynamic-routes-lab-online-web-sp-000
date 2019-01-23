0f2dfe1c394e3abbe710c1c1ab2fc4a56b898865
Aaron Berns <anberns@gmail.com>
2019-01-23 14:57:32 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,9 @@ class Application
     
     if req.path.match(/items/)
       item = req.path.split("/items/").last
-      if item_instance =@@items.find{|i| i.name == item }
+      item_instance =@@items.find{|i| i.name == item }
+      
+      if item_instance != nil
         resp.write item_instance.price
         resp.status = 200
       else 
@@ -20,6 +22,30 @@ class Application
       resp.write "Route not found"
       resp.status = 404
     end
+    resp.finish
   end
 end
 
+class Application
+
+  @@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      if item =@@items.find{|i| i.name == item_name}
+        resp.write item.price
+      else 
+        resp.status = 400
+        resp.write "Item not found"
+      end
+    else
+      resp.status=404
+      resp.write "Route not found"
+    end
+    resp.finish
+  end
+
+end
